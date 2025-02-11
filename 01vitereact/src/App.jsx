150b23e9cc23bad6ca33759314af6e930451b33a
Ankit <ankitspare981@gmail.com>
2025-02-11 11:17:03 +0530

    interview

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Chai from "./chai"
+function App() {
+  const username="chai aur code"
+  return (
+    <>
+      {/* <h1> Chai aur Code with vite| Ankit</h1> */}
+      <Chai/>
+      <h1>chai aur react {username}</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+function MyApp()
+{
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+// const reactElement={
+//   type: 'a',
+//   props:{
+//       href:'https://google.com',
+//       target:'_blank'
+//   },
+//   children:'Click me to visit google'
+// }
+const anotherUser="chai aur react"
+const reactElement=React.createElement(
+    'a',
+  {
+    href:'https://google.com',
+    target:'_blank'
+  },
+  'click me to visit Google',
+  anotherUser
+)
+const anotherElement=(
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
+)
